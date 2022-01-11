b86d0a1de851cddc49ede9131c9e70e4d7bca0d1
神仙梨子 <626993211@qq.com>
2022-01-11 17:34:37 +0800

    [fix]Fix TS conflicts

--- packages/eslint-config-vue3/index.js
+++ packages/eslint-config-vue3/index.js
@@ -3,11 +3,7 @@ module.exports = {
     '@antfu/vue'
   ],
   rules: {
-    'comma-dangle': ['error', 'never'],
     'curly': ['error', 'multi-line'],
-    'brace-style': ['error', '1tbs', {
-      'allowSingleLine': true
-    }],
     'no-restricted-syntax': [
       'error',
       'DebuggerStatement',
@@ -18,6 +14,11 @@ module.exports = {
       'anonymous': 'never',
       'named': 'never',
       'asyncArrow': 'always'
-    }]
+    }],
+
+    'comma-dangle': 'off',
+    '@typescript-eslint/comma-dangle': ['error', 'never'],
+    'brace-style': 'off',
+    '@typescript-eslint/brace-style': ['error', '1tbs', { allowSingleLine: true }]
   }
 }
