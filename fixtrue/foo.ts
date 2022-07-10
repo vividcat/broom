bfdacf3bf07e5525e0b086aaae5bb8d14d5bd200
hezulong1 <626993211@qq.com>
2022-07-11 00:57:16 +0800

    test: quote-props

--- fixtrue/foo.ts
+++ fixtrue/foo.ts
@@ -5,7 +5,7 @@ if(bar111==2) {
   console.log(foo**2)
 }
 else {alert(foo)}
-debugger
+
 switch(foo){
 
   case 1:
@@ -13,8 +13,14 @@ switch(foo){
 }
 
 var aList = [1.3,2.4,5.5,]
-var oMap={n:1,   o: 2};
-var oMap1={n:1,o:2,u:3,}
+var oMap={n:1,   o: 2, 2e2: '200','2e2': '200' };
+var rule={
+  class:1,
+  async: "2e2",
+  err() {
+    console.error('error')
+  }
+}
 
 interface T{
   name:string;
